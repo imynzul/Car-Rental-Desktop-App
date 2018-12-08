907705ea50a0e8c66955fd5acada47b418c4be47
imynzul <imynzul@gmail.com>
2018-12-08 10:42:20 +0300

    Get added

--- src/com/home/homework13/gui/StartFrame.java
+++ src/com/home/homework13/gui/StartFrame.java
@@ -8,13 +8,18 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-    public class StartFrame extends JFrame {
+import static java.awt.BorderLayout.*;
 
-        private JPanel panel;
+public class StartFrame extends JFrame {
+
+        private JPanel panelBackground, panel, panel1;
         private JTextField url, login, name;
         private JPasswordField password;
         private JLabel labelUrl, labelName, labelLogin, labelPassword;
         private JButton create, delete, connect;
+        private JMenuBar bar;
+        private JMenuItem item1;
+        private JMenu menu;
 
         public StartFrame(){
             setSize(310,400);
@@ -24,14 +29,27 @@ import java.awt.event.ActionListener;
             setResizable(false);
 
             initComponents();
-
             activation();
+            initComponentsLocation();
 
             setVisible(true);
         }
 
         public void initComponents(){
+            panelBackground = new JPanel();
             panel = new JPanel();
+            panel1 = new JPanel();
+
+            panelBackground.setLayout(new BorderLayout());
+            panel.setBackground(Color.LIGHT_GRAY);
+            panel1.setBackground(Color.DARK_GRAY);
+            panel1.setPreferredSize(new Dimension(0, 10));
+
+            bar = new JMenuBar();
+
+            menu = new JMenu("Файл");
+
+            item1 = new JMenuItem("Выйти");
 
             url = new JTextField("jdbc:mysql://127.0.0.1/",25);
             name = new JTextField("car_rent",25);
@@ -45,10 +63,13 @@ import java.awt.event.ActionListener;
 
             create = new JButton("Создать");
             create.setPreferredSize(new Dimension(280,20));
+            create.setBackground(Color.decode("#85c888"));
             delete = new JButton("Удалить");
             delete.setPreferredSize(new Dimension(280, 20));
+            delete.setBackground(Color.decode("#85c888"));
             connect = new JButton("Подключиться");
             connect.setPreferredSize(new Dimension(280, 20));
+            connect.setBackground(Color.decode("#c89085"));
 
 
             panel.add(labelUrl);
@@ -64,7 +85,20 @@ import java.awt.event.ActionListener;
             panel.add(delete);
             panel.add(connect);
 
-            add(panel);
+
+
+            add(panelBackground);
+        }
+
+        public void initComponentsLocation(){
+
+            panelBackground.add(panel, BorderLayout.CENTER);
+            panelBackground.add(panel1, BorderLayout.NORTH);
+
+            menu.add(item1);
+            bar.add(menu);
+
+            setJMenuBar(bar);
         }
 
         public void activation(){
@@ -89,6 +123,13 @@ import java.awt.event.ActionListener;
                     DB db = new DB(url.getText(), name.getText(), login.getText(), String.valueOf(password.getPassword()));
                     new Authorization(db);
                     dispose();
+                }
+            });
+
+            item1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
                 }
             });
 
